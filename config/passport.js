22804a1f8048c95141213e1c28be151a3a6515d8
srikk <122169588+py-code314@users.noreply.github.com>
2026-03-10 19:45:22 -0400

    feat: Add passport local strategy for authentication
    - Store session data in db while logging in.
    - Retrieve user data from db with every request.

---
+++ config/passport.js
@@ -0,0 +1,51 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+// const pool = require('./database')
+const bcrypt = require('bcryptjs')
+
+
+/* Verify username and password before logging in */
+const verifyCallback = async (username, password, done) => {
+  try {
+    const { rows } = await pool.query(
+      'SELECT * FROM users WHERE username = $1',
+      [username],
+    )
+    const user = rows[0]
+
+    if (!user) {
+      return done(null, false, { message: 'Incorrect username' })
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) {
+      // Passwords don't match
+      return done(null, false, { message: 'Incorrect password' })
+    }
+    // Username and password match
+    return done(null, user)
+  } catch (err) {
+    return done(err)
+  }
+}
+
+const strategy = new LocalStrategy(verifyCallback)
+
+passport.use(strategy)
+
+// Store session data in db
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+// Retrieve user data from db
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [id])
+    const user = rows[0]
+
+    done(null, user)
+  } catch (err) {
+    done(err)
+  }
+})
