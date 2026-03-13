fd88fe6a81de698c205c741c609146c8604a43fc
srikk <122169588+py-code314@users.noreply.github.com>
2026-03-13 16:14:11 -0400

    feat: Validate and sanitize new message

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,3 +1,15 @@
+/* Imports */
+const { body, validationResult, matchedData } = require('express-validator')
+
+/* Error messages */
+const emptyErr = 'can not be empty.'
+
+/* Validate log in data */
+const validateMessage = [
+  body('title').trim().notEmpty().withMessage(`Title ${emptyErr}`),
+  body('content').trim().notEmpty().withMessage(`Message ${emptyErr}`),
+]
+
 /* Show new message page */
 async function message_get(req, res) {
   res.render('pages/message', {
