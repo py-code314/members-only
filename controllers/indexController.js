cd6eba0c75e9e9483f4ed15f3d4c2c69f925a2c0
srikk <122169588+py-code314@users.noreply.github.com>
2026-03-24 10:41:53 -0400

    style: Update form styles
    - Delete files and folders related to index.

--- controllers/indexController.js
+++
@@ -1,8 +0,0 @@
-/* Show index page */
-async function index_page_get(req, res) {
-  res.render('pages/index', {
-    title: 'Index',
-  })
-}
-
-module.exports = index_page_get 
--- routes/indexRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const indexRouter = express.Router()
-const index_page_get  = require('../controllers/indexController')
-
-// Index page route
-indexRouter.get('/', index_page_get)
-
-module.exports = indexRouter
